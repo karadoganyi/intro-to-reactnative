40ccb63c2d6b64a42f6bd213e4871a03dfc152ad
karadogan <yusufkaradogan@karadogans-MacBook-Air.local>
2023-02-03 15:10:00 +0300

    forms

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,12 @@
 import React, {useState, useCallback, useEffect} from 'react';
-import {FlatList, StyleSheet} from 'react-native';
+import {FlatList, StyleSheet, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import PalettePreview from '../components/PalettePreview';
 
-const Home = ({navigation}) => {
+const Home = ({navigation, route}) => {
+  const newColorPalette = route.params
+    ? route.params.newColorPalette
+    : undefined;
   const [colorPalettes, setColorPalettes] = useState([]);
   const [isRefreshing, setIsRefreshing] = useState(false);
 
@@ -29,6 +33,12 @@ const Home = ({navigation}) => {
     }, 500);
   });
 
+  useEffect(() => {
+    if (newColorPalette) {
+      setColorPalettes(palettes => [newColorPalette, ...palettes]);
+    }
+  }, [newColorPalette]);
+
   return (
     <FlatList
       style={styles.list}
@@ -42,6 +52,14 @@ const Home = ({navigation}) => {
       )}
       refreshing={isRefreshing}
       onRefresh={handleRefresh}
+      ListHeaderComponent={
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('ColorPaletteModal');
+          }}>
+          <Text style={styles.heading}>Add a Color Scheme</Text>
+        </TouchableOpacity>
+      }
     />
   );
 };
@@ -50,6 +68,12 @@ const styles = StyleSheet.create({
   list: {
     padding: '10',
     backgroundColor: 'white',
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'teal',
   },
 });
 
